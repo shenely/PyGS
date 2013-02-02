f824d240b156c205043ea48df738311113523645
Sean Henely <sean.henely@gmail.com>
2013-02-02 16:48:20 -0500

    I think I'm about to remove the old stuff

--- src/routine/__init__.py
+++
@@ -1,9 +0,0 @@
-def coroutine(func):
-    def wrapper(*args,**kw):
-        gen = func(*args, **kw)
-        gen.next()
-        return gen
-    wrapper.__name__ = func.__name__
-    wrapper.__dict__ = func.__dict__
-    wrapper.__doc__  = func.__doc__
-    return wrapper
