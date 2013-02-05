ee781757f03fb30cbe18459460b8b1bc8ee9d155
shenely <shenely@X-Hen>
2013-02-05 00:16:52 -0500

    Paying off some technical debt by documenting (more to come)

--- src/clock/epoch/routine.py
+++ src/clock/epoch/routine.py
@@ -4,9 +4,23 @@
 
 Author(s):  Sean Henely
 Language:   Python 2.x
-Modified:   02 February 2013
+Modified:   05 February 2013
 
-Purpose:    
+Provides routines for manipulating epoch data.
+
+Functions:
+update -- Update system epoch
+format -- Format epoch message
+parse  -- Parse epoch message
+
+"""
+
+"""Change log:
+                                        
+Date          Author          Version     Description
+----------    ------------    --------    -----------------------------
+2013-02-05    shenely         1.0         Promoted to version 1.0
+
 """
 
 
@@ -33,7 +47,7 @@ from .message import EpochMessage
 #
 __all__ = ["update",
            "format",
-           "process"]
+           "parse"]
 #
 ##################
 
@@ -41,68 +55,186 @@ __all__ = ["update",
 ####################
 # Constant section #
 #
-__version__ = "0.1"#current version [major.minor]
-
-EPOCH_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+__version__ = "1.0"#current version [major.minor]
 #
 ####################
 
 
 @coroutine
 def update(system,pipeline=None):
-    """Update System Epoch"""
+    """Story:  Update system epoch
     
+    IN ORDER TO manage the system state
+    AS THE generic segment
+    I WANT TO keep the system epoch synchronized with an upstream 
+        source
+        
+    """
+    
+    """Specification:  Update system epoch
+    
+    Scenario 1:  Epoch update created
+    GIVEN a system state with epoch defined
+        AND a pipeline to send epoch to (default None)
+    WHEN a epoch update is created
+    THEN a epoch update SHALL be activated
+        
+    Scenario 2:  Upstream epoch received
+    GiVEN an active epoch update
+    WHEN an epoch is received from an upstream source
+    THEN the system epoch SHALL be set to the received epoch
+        AND the received epoch SHALL be sent downstream
+    
+    Scenario 3:  Epoch update destroyed
+    GiVEN an active epoch update
+    WHEN the epoch update is destroyed
+    THEN the downstream routine SHALL be destroyed
+        AND the epoch update SHALL deactivated
+    
+    """
+    
+    #Configuration validation
     assert isinstance(system,EpochState)
     assert isinstance(pipeline,types.GeneratorType) or pipeline is None
     
+    logging.info("Epoch.Update:  Starting")
     while True:
-        epoch = yield system,pipeline
-        
-        assert isinstance(epoch,datetime)
-        
-        system.epoch = epoch
-                        
-        logging.info("Ground.Epoch:  Updated to %s" % system.epoch)
+        try:
+            epoch = yield system,pipeline
+        except GeneratorExit:
+            logging.info("Epoch.Update:  Stopping")
+            
+            #close downstream routine (if it exists)
+            pipeline.close() if pipeline is not None else None
+            
+            return
+        else:        
+            #input validation
+            assert isinstance(epoch,datetime)
+            
+            system.epoch = epoch
+                            
+            logging.debug("Epoch.Update:  Updated to %s" % system.epoch)
 
 @coroutine
 def format(address,pipeline=None):
-    """Format Epoch Message"""
+    """Story:  Format epoch message
     
+    IN ORDER TO generate messages for distributing the current epoch
+    AS THE clock segment
+    I WANT TO encode the epoch in a defined string format
+        
+    """
+    
+    """Specification:  Format epoch message
+    
+    Scenario 1:  Epoch formatter created
+    GIVEN an address for the message envelope
+        AND a pipeline to send message to (default None)
+    WHEN a epoch formatter is created
+    THEN a epoch formatter SHALL be activated
+        
+    Scenario 2:  Upstream epoch received
+    GiVEN an active epoch formatter
+    WHEN an epoch is received from an upstream source
+    THEN the epoch SHALL be encoded as a message
+        AND the encoded message SHALL be sent downstream
+    
+    Scenario 3:  Epoch formatter destroyed
+    GiVEN an active epoch formatter
+    WHEN the epoch formatter is destroyed
+    THEN the downstream routine SHALL be destroyed
+        AND the epoch formatter SHALL deactivated
+    
+    """
+    
+    #Configuration validation
     assert isinstance(address,types.StringTypes)
     assert isinstance(pipeline,types.GeneratorType) or pipeline is None
     
     message = None
+    
+    logging.info("Epoch.Format:  Starting")
     while True:
-        epoch = yield message,pipeline
-        
-        assert isinstance(epoch,datetime)
-        
-        notice = EpochMessage(epoch)
-        message = address,encoder(notice)
-        
-        logging.info("Ground.Epoch:  Formatted as %s" % notice.params.epoch)
+        try:
+            epoch = yield message,pipeline
+        except GeneratorExit:
+            logging.info("Epoch.Format:  Stopping")
+            
+            #close downstream routine (if it exists)
+            pipeline.close() if pipeline is not None else None
+            
+            return
+        else:        
+            #input validation
+            assert isinstance(epoch,datetime)
+            
+            notice = EpochMessage(epoch)
+            message = address,encoder(notice)
+                            
+            logging.debug("Epoch.Format:  Formatted as %s" % notice.params.epoch)
 
 @coroutine
 def parse(pipeline=None):
-    """Parse Epoch Message"""
+    """Story:  Parse epoch message
     
+    IN ORDER TO process messages for synchronizing the current epoch
+    AS THE generic segment
+    I WANT TO decode the a formatted string as an epoch
+        
+    """
+    
+    """Specification:  Parse epoch message
+    
+    Scenario 1:  Epoch parser created
+    GIVEN a pipeline to send epoch to (default None)
+    WHEN a epoch parser is created
+    THEN a epoch parser SHALL be activated
+        
+    Scenario 2:  Upstream message received
+    GiVEN an active epoch parser
+    WHEN an message is received from an upstream source
+    THEN the message SHALL be decoded as an epoch
+        AND the decoded epoch SHALL be sent downstream
+    
+    Scenario 3:  Epoch parser destroyed
+    GiVEN an active epoch parser
+    WHEN the epoch parser is destroyed
+    THEN the downstream routine SHALL be destroyed
+        AND the epoch parser SHALL deactivated
+    
+    """
+    
+    #Configuration validation
     assert isinstance(pipeline,types.GeneratorType) or pipeline is None
     
     epoch = None
+    
+    logging.info("Epoch.Parse:  Starting")
     while True:
-        address,message = yield epoch,pipeline
+        try:
+            address,message = yield epoch,pipeline
+        except GeneratorExit:
+            logging.info("Epoch.Parse:  Stopping")
+            
+            #close downstream routine (if it exists)
+            pipeline.close() if pipeline is not None else None
+            
+            return
+        else:        
+            assert isinstance(message,types.StringTypes)
         
-        assert isinstance(message,types.StringTypes)
-        
-        notice = decoder(message)
-        
-        assert hasattr(notice,"method")
-        assert notice.method == "epoch"
-        assert hasattr(notice,"params")
-        assert isinstance(notice.params,ObjectDict)
-        assert hasattr(notice.params,"epoch")
-        assert isinstance(notice.params.epoch,datetime)
-        
-        epoch = notice.params.epoch
-                
-        logging.info("Ground.Epoch:  Parsed as %s" % notice.params.epoch)
+            notice = decoder(message)
+            
+            #Output validation
+            #NOTE:  this should be handled by the RequestMessage class
+            assert hasattr(notice,"method")
+            assert notice.method == "epoch"
+            assert hasattr(notice,"params")
+            assert isinstance(notice.params,ObjectDict)
+            assert hasattr(notice.params,"epoch")
+            assert isinstance(notice.params.epoch,datetime)
+            
+            epoch = notice.params.epoch
+                            
+            logging.debug("Epoch.Parse:  Parsed as %s" % notice.params.epoch)
--- src/clock/routine.py
+++ src/clock/routine.py
@@ -4,9 +4,21 @@
 
 Author(s):  Sean Henely
 Language:   Python 2.x
-Modified:   02 February 2013
+Modified:   05 February 2013
 
-Purpose:    
+Provides routines for driving the simulation clock.
+
+Functions:
+continuous -- Continuous clock routine
+discrete   -- Discrete clock routine
+"""
+
+"""Change log:
+                                        
+Date          Author          Version     Description
+----------    ------------    --------    -----------------------------
+2013-02-05    shenely         1.0         Promoted to version 1.0
+
 """
 
 
@@ -38,46 +50,160 @@ __all__ = ["continuous",
 ####################
 # Constant section #
 #
-__version__ = "0.1"#current version [major.minor]
+__version__ = "1.0"#current version [major.minor]
 
-J2000 = datetime(2000,1,1,12)
+J2000 = datetime(2000,1,1,12)#Julian epoch (2000-01-01T12:00:00Z)
 
-CLOCK_SCALE = 1.0
-CLOCK_STEP = timedelta(seconds=60)
+CLOCK_SCALE = 1.0#Clock rate scale (default 1:1, i.e. real-time)
+CLOCK_STEP = timedelta(seconds=60)#Clock step (default to 60 seconds)
 #
 ####################
 
 
+class ModifyClockScale(Exception):
+    def __init__(self,value):
+        Exception.__init__(self,value)
+        
+        assert isinstance(value,(types.IntType,types.FloatType))
+        
+        self.scale = value
+        
+class ModifyClockStep(Exception):
+    def __init__(self,value):
+        Exception.__init__(self,value)
+        
+        assert isinstance(value,timedelta)
+        
+        self.step = value
+
 @coroutine
 def continuous(epoch=J2000,scale=CLOCK_SCALE,pipeline=None):
-    """Continuous System Epoch"""
+    """Story:  Continuous clock routine
     
+    IN ORDER TO manage the simulation time
+    AS THE clock segment
+    I WANT TO progress the simulation time based off the progress of
+        system time
+    
+    """
+    
+    """Specification:  Continuous clock routine
+    
+    Scenario 1:  Continuous clock created
+    GIVEN an initial epoch (default J2000.0)
+        AND a clock rate scale (default 1.0)
+        AND a pipeline to send current time to (default None)
+    WHEN a continuous clock is created
+    THEN a continuous clock SHALL be activated
+        
+    Scenario 2:  Simulation time requested
+    GiVEN an active continuous clock
+    WHEN the clock value is requested
+    THEN the system time since the last request SHALL be determined
+        AND the elapsed time SHALL be scaled by the clock rate scale
+        AND the simulation time SHALL be increased by the elapsed time
+        AND the response SHALL be the simulation time
+    
+    Scenario 3:  Clock rate scale modified
+    GiVEN an active continuous clock
+    WHEN a clock rate scale modification is requested
+    THEN the clock scale rate SHALL be modified to the requested value
+        AND the simulation time SHALL be requested
+    
+    Scenario 4:  Continuous clock destroyed
+    GiVEN an active continuous clock
+    WHEN the continuous clock is destroyed
+    THEN the downstream routine SHALL be destroyed
+        AND the continuous clock SHALL deactivated
+    
+    """
+    
+    #Configuration validation
     assert isinstance(epoch,datetime)
     assert isinstance(scale,(types.IntType,types.FloatType))
     assert isinstance(pipeline,types.GeneratorType) or pipeline is None
     
     now = datetime.utcnow()
+        
+    logging.info("Clock.Continuous:  Stating at %s" % epoch)
     while True:
-        yield epoch,pipeline
-        
-        past = now
-        now = datetime.utcnow()
-        
-        epoch += scale * (now - past)
-        
-        logging.info("Clock.Continuous:  Updated epoch to %s" % epoch)
+        try:
+            yield epoch,pipeline
+        except ModifyClockScale as error:           
+            scale = error.scale
+        except GeneratorExit:
+            logging.info("Clock.Continuous:  Stopping at %s" % epoch)
+            
+            #close downstream routine (if it exists)
+            pipeline.close() if pipeline is not None else None
+            
+            return
+        finally:
+            past = now
+            now = datetime.utcnow()
+            
+            epoch += scale * (now - past)#increase simulation time
+            
+            logging.debug("Clock.Continuous:  Updated epoch to %s" % epoch)
 
 @coroutine
 def discrete(epoch=J2000,step=CLOCK_STEP,pipeline=None):
-    """Discrete System Epoch"""
+    """Story:  Discrete clock routine
     
+    IN ORDER TO manage the simulation time
+    AS THE clock segment
+    I WANT TO progress the simulation time at fixed intervals
+    
+    """
+    
+    """Specification:  Discrete clock routine
+    
+    Scenario 1:  Discrete clock created
+    GIVEN an initial epoch (default J2000.0)
+        AND a clock step (default 60 seconds)
+        AND a pipeline to send current time to (default None)
+    WHEN a discrete clock is created
+    THEN a discrete clock SHALL be activated
+        
+    Scenario 2:  Simulation time requested
+    GiVEN an active discrete clock
+    WHEN the clock value is requested
+    THEN the simulation time SHALL be increased by the clock step size
+        AND the simulation time SHALL be sent downstream
+    
+    Scenario 3:  Clock step size modified
+    GiVEN an active discrete clock
+    WHEN a clock step size modification is requested
+    THEN the clock step size SHALL be modified to the requested value
+        AND the simulation time SHALL be requested
+    
+    Scenario 4:  Discrete clock destroyed
+    GiVEN an active discrete clock
+    WHEN the discrete clock is destroyed
+    THEN the downstream routine SHALL be destroyed
+        AND the discrete clock SHALL deactivated
+    
+    """
+    
+    #Configuration validation
     assert isinstance(epoch,datetime)
     assert isinstance(step,timedelta)
     assert isinstance(pipeline,types.GeneratorType) or pipeline is None
-    
+        
+    logging.info("Clock.Discrete:  Stating at %s" % epoch)
     while True:
-        yield epoch,pipeline
-        
-        epoch += step
-        
-        logging.info("Clock.Discrete:  Updated epoch to %s" % epoch)
+        try:
+            yield epoch,pipeline
+        except ModifyClockStep as error:
+            step = error.step
+        except GeneratorExit:
+            logging.info("Clock.Discrete:  Stopping at %s" % epoch)
+            
+            #close downstream routine (if it exists)
+            pipeline.close() if pipeline is not None else None
+            
+            return
+        finally:
+            epoch += step#increase simulation time
+            
+            logging.debug("Clock.Discrete:  Updated epoch to %s" % epoch)
